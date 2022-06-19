688070cac5f8c3169cd66ad58f0d9e5ba06942d1
deeprecommend <deeprecommend@gmail.com>
2022-06-20 07:56:40 +0900

    delete app title

--- lib/main.dart
+++ lib/main.dart
@@ -40,15 +40,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: materialWhite,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
